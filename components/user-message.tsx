f69eebf0759df66473190feb176b7bb3bde5fd31
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-06-19 01:26:58 +0200

    feat: enable share by default

--- components/user-message.tsx
+++ components/user-message.tsx
@@ -1,21 +1,20 @@
 import { ChatShare } from "./chat-share";
 
 interface UserMessageProps {
-  message: string;
   chatId?: string;
+  message: string;
   showShare?: boolean;
 }
 
 export const UserMessage: React.FC<UserMessageProps> = ({
-  message,
   chatId,
+  message,
   showShare = false,
 }) => {
-  const enableShare = process.env.ENABLE_SHARE === "true";
   return (
     <div className="mt-2 flex min-h-10 w-full items-center space-x-1">
       <div className="w-full flex-1 break-words text-xl">{message}</div>
-      {enableShare && showShare && chatId && <ChatShare chatId={chatId} />}
+      {showShare && chatId && <ChatShare chatId={chatId} />}
     </div>
   );
 };
